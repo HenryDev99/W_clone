314ec35d2f94ea119000a85adfe6333c7bffbe9e
frida <frida921230@gmail.com>
2023-06-13 22:37:39 +0900

    button 수평정렬

--- src/pages/PreShow_jsy/PreShowLayout_jsy.tsx
+++ src/pages/PreShow_jsy/PreShowLayout_jsy.tsx
@@ -1,6 +1,6 @@
 import Button from "antd/es/button";
-import button from "antd/es/button";
 import Pagination from "antd/es/pagination";
+import "/Users/fridajeong/W_clone/src/pages/PreShow_jsy/PreShow_jsy.scss";
 
 const PreShowLayout_JSY:React.FC = ()=>{
 
@@ -73,6 +73,44 @@ const PreShowLayout_JSY:React.FC = ()=>{
                         </Button>
                     </li>
                 </ul>
+                <ul >
+                    <li>
+                        <Button shape="round">
+                            #
+                            All
+                        </Button>
+                    </li>
+                    <li>
+                        <Button shape="round">
+                            #
+                            모던
+                        </Button>
+                    </li>
+                    <li>
+                        <Button shape="round">
+                            #
+                            페미닌
+                        </Button>
+                    </li>
+                    <li>
+                        <Button shape="round">
+                            #
+                            캐주얼
+                        </Button>
+                    </li>
+                    <li>
+                        <Button shape="round">
+                            #
+                            베이직
+                        </Button>
+                    </li>
+                    <li>
+                        <Button shape="round">
+                            #
+                            로맨틱
+                        </Button>
+                    </li>
+                </ul>
             </div>
             <div className="preshow-content">
                 <div className="event-list-wrap">
@@ -91,6 +129,7 @@ const PreShowLayout_JSY:React.FC = ()=>{
                         </ul>
                     </div>
                 </div>
+            
             </div>
             <Pagination style={{ marginTop:"60px" }} defaultCurrent={1} total={50} />
 
